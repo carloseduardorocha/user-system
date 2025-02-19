6016b40cc854bd2c3a22284a5aa35fdf9234435d
Carlos Rocha <carlos.rocha548@hotmail.com>
2025-02-19 20:33:27 -0300

    feat: created me, update and update-password routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\JwtMiddleware;
+
 use App\Http\Controllers\UserController;
 
 use Illuminate\Support\Facades\Route;
@@ -15,6 +17,15 @@ Route::prefix('v1')->group(function() {
                 Route::post('/create', 'create')->name('create');
                 Route::delete('/delete', 'delete')->name('delete');
                 Route::patch('/restore', 'restore')->name('restore');
+            });
+        });
+    });
+    Route::middleware([JwtMiddleware::class])->group(function() {
+        Route::prefix('')->name('users.')->group(function() {
+            Route::controller(UserController::class)->group(function() {
+                Route::get('/me', 'me')->name('me');
+                Route::put('/update', 'update')->name('update');
+                Route::patch('/update-password', 'updatePassword')->name('update.password');
             });
         });
     });
