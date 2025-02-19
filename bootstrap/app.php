d14f9030bf54eaa247a035b1c36f098730e7e434
Carlos Rocha <carlos.rocha548@hotmail.com>
2025-02-18 22:17:42 -0300

    feat: added handling to unnpreparated exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Helpers\Logger;
+use App\Helpers\ApiResponse;
+
+use Illuminate\Http\Response;
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -20,5 +25,34 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function(Exceptions $exceptions) {
+        $exceptions->render(function(Throwable $e, $request) {
+            if (!config('app.debug'))
+            {
+                if ($request->expectsJson())
+                {
+                    $known_errors = [
+                        'Unauthenticated.' => Response::HTTP_UNAUTHORIZED,
+                        'Unauthenticated'  => Response::HTTP_UNAUTHORIZED,
+                    ];
 
+                    Logger::critical(
+                        'Generic Exception Errors',
+                        [
+                            'message' => addslashes($e->getMessage()),
+                            'line'    => $e->getLine(),
+                            'file'    => $e->getFile(),
+                            'code'    => $e->getCode(),
+                        ],
+                        ['GENERAL_EXCEPTIONS'],
+                        $e
+                    );
+
+                    return ApiResponse::jsonException(
+                        $known_errors[addslashes($e->getMessage())] ?? Response::HTTP_INTERNAL_SERVER_ERROR,
+                        addslashes($e->getMessage())                ?? 'Server API Error',
+                        class_basename($e)
+                    );
+                }
+            }
+        });
     })->create();
